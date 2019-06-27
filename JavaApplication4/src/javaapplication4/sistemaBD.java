93c81b04f9485c6530f0f7d2a38622ea1c2a286a
Gaviota Antonia Herrera Prieto <35613309+GaviotaAntonia@users.noreply.github.com>
2019-06-27 10:55:18 -0500

    nuevo

---
+++ JavaApplication4/src/javaapplication4/sistemaBD.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class sistemaBD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      LOGEO nuevo = new LOGEO();//Para mandar a llamar el JFrame Inicio
+        nuevo.setVisible(true);
+        
+    }
+    
+}
