5893403a288dcf5707a2bc6d3de96eecd0c8b322
EnriqueOrozco <51837209+EnriqueOrozco@users.noreply.github.com>
2019-07-10 15:06:35 -0500

    nuevos cambios para la conexion

---
+++ JavaApplication4/src/javaapplication4/conexionsql.java
@@ -0,0 +1,30 @@
+package javaapplication4;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import javax.swing.Icon;
+
+public class conexion {
+    Connection con=null;
+    
+    public Connection conexion(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/dbdistribuida","root","");
+           // System.out.println("conexion exitosa");
+           JOptionPane.showMessageDialog(null, "Conexion Exitosa", "Conexion Exitosa",JOptionPane.INFORMATION_MESSAGE, new ImageIcon("src\\iconos\\success.png"));
+        } 
+        catch (ClassNotFoundException| SQLException e) 
+        {
+            System.out.println("error de conexion"+e);
+            JOptionPane.showMessageDialog(null,"error"+e);
+        }
+     return con;
+    }
+
+    Connection getConnection() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
